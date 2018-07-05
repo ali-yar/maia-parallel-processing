3ac0febedb612fa79e3b1fb12571a52995916bda
ali-yar <32914277+ali-yar@users.noreply.github.com>
2018-07-06 01:48:55 +0200

    updated Matlab code

--- Project/Matlab/unfiltered_backprojection_indices.m
+++ Project/Matlab/unfiltered_backprojection_indices.m
@@ -5,15 +5,14 @@ clc; clear; close all;
 P = phantom(256); 
 figure; imshow(P);
 
-N = 90;
+N = 180;
 step = 180/N;
 thetas = 0:step:179;
 
 [R,xp] = radon(P,thetas);
-figure; imagesc(thetas,xp,R); colormap(gray);
 
 sinogram = R;
-% sinogram = imread('sinogram.jpg');
+figure; imshow(sinogram,[]);
 
 numOfParallelProjections = size(sinogram,1);
 numOfAngularProjections  = length(thetas); 
@@ -27,28 +26,47 @@ BPI = zeros(numOfParallelProjections,numOfParallelProjections);
 % find the middle index of the projections
 midindex = floor(numOfParallelProjections/2) + 1;
 
+% set up filter
+filterMode = 'sheppLogan'; % 'sheppLogan' or 'ramLak'
 
-figure;
+if mod(numOfParallelProjections,2) == 0
+    halfFilterSize = floor(1 + numOfParallelProjections);
+else
+    halfFilterSize = floor(numOfParallelProjections);
+end
+
+if strcmp(filterMode,'ramLak')
+    filter = zeros(1,halfFilterSize);
+    filter(1:2:halfFilterSize) = -1./([1:2:halfFilterSize].^2 * pi^2);
+    filter = [fliplr(filter) 1/4 filter];
+elseif strcmp(filterMode,'sheppLogan')
+    filter = -2./(pi^2 * (4 * (-halfFilterSize:halfFilterSize).^2 - 1) );
+end
+
+% convolve sinogram with filter
+sinogramFiltered = zeros(size(sinogram));
+for i = 1:numOfAngularProjections
+    sinogramFiltered(:,i) = conv(sinogram(:,i),filter,'same');
+end
+
 % loop over each projection
 for r = 1:size(BPI,1)
   for c = 1:size(BPI,2)
     for i = 1:numOfAngularProjections
         t = thetas(i);
         x = c - midindex;
-        y = r - midindex;
-        % figure out which projections to add to which spots
-        rotCoords = round(midindex + x*sin(t) + y*cos(t));
+        y = - (r - midindex);
+        
+		% transformation
+        rotCoords = midindex + round(x*cos(t) + y*sin(t));
 
-        % check which coords are in bounds
+        % check boundaries
         if ((rotCoords > 0) && (rotCoords <= numOfParallelProjections))
-            % summation
-            BPI(r,c) = BPI(r,c) + sinogram(rotCoords,i)./numOfAngularProjections;
+            % update BPI
+            BPI(r,c) = BPI(r,c) + sinogramFiltered(rotCoords,i)./numOfAngularProjections;
         end
-        % visualization on the fly
-
     end
   end
 end
-im = mat2gray(BPI);
-im = imrotate(im,90);
-imshow(im);
+
+figure; imshow(BPI,[]);
