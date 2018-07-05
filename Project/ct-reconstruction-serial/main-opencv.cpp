37a583a139d944c054a1c0044c604d6e77daedca
ali-yar <32914277+ali-yar@users.noreply.github.com>
2018-07-06 01:15:01 +0200

    Updated serial code

--- Project/ct-reconstruction-serial/main-opencv.cpp
+++ Project/ct-reconstruction-serial/main-opencv.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <math.h>
+#include <ctime>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -8,8 +9,15 @@
 
 #define PI 3.14159265358979323846
 
-const int sinogramHeight = 367;
-const int sinogramWidth = 90;
+//im -> sino : 25 -> 39 / 256 -> 367 / 512 -> 729 / 768 -> 1091 /1024 -> 1453
+const int sinogramHeight = 367; //367;
+const int sinogramWidth = 180;
+
+bool readFromOpencv = !true;
+bool filteredBP = true;
+
+std::clock_t start;
+double duration;
 
 
 /************************
@@ -28,10 +36,23 @@ void saveToFile(std::string filename, T mat[][cols], size_t rows);
 template<typename T, size_t cols>
 void convertToMat(T mat[][cols], cv::Mat &dst, size_t rows, int type, bool normalize=false);
 
-/* Display matrix as image */
+/* Display 2D array as image */
 template<typename T, size_t cols>
 void cvimshow(T mat[][cols], size_t rows, bool flip=false, std::string title="image", int waitKey=0);
 
+/* Save sinogram to text file */
+template<typename T, size_t cols>
+void saveSinogramAsFile(std::string filename, T mat[][cols], size_t rows);
+
+/* build a Shepp Logan 1D filter */
+void getFitler(float* a, int s);
+
+/* 1D central convolution. Similar to Matlab's conv(a,b,'same') with a < b */
+void centralConvolution(float* a, float* b, float* c);
+
+/* Filter sinogram */
+template<typename T, size_t cols>
+void filterSinogram (T src[][cols], T dst[][cols], size_t rows);
 
 /**************************
  * Program starting point *
@@ -39,15 +60,46 @@ void cvimshow(T mat[][cols], size_t rows, bool flip=false, std::string title="im
 
 int main() {
     // where to read and write data
-    std::string inputFile = "sinogram.txt";
+    std::string inputFile = "sinogram_256.txt";
     std::string outputFile = "reconstructed.txt";
 
-    // load sinogram
+
     float sinogram[sinogramHeight][sinogramWidth];
-    loadFromFile(inputFile, sinogram, sinogramHeight);
+    float sinogramFiltered[sinogramHeight][sinogramWidth];
+
+    if (readFromOpencv) {
+        cv::Mat sinoMat = cv::imread("C:/Users/hp4540/Desktop/pps/sinoim_256.tif",CV_LOAD_IMAGE_UNCHANGED);
+        cv::imshow("sinoim", sinoMat); cv::waitKey(0);
+        double max = 0.0;
+        cv::minMaxLoc(sinoMat,0,&max);
+        std::cout << "max = " << max << std::endl;
+        std::cout << "sinoMat.rows = " << sinoMat.rows << std::endl;
+        std::cout << "sinoMat.cols = " << sinoMat.cols << std::endl;
+        for(int i=0; i<sinoMat.rows; i++) {
+            for(int j=0; j<sinoMat.cols; j++) {
+                sinogram[i][j] = sinoMat.at<ushort>(i,j);
+            }
+        }
+       //saveSinogramAsFile("sinogram_145-90.txt", sinogram, sinoMat.rows); return 0;
+    } else {
+        // load sinogram
+        loadFromFile(inputFile, sinogram, sinogramHeight);
+    }
 
     // display the sinogram
     cvimshow(sinogram, sinogramHeight, false, "sinogram");
+
+    if (filteredBP) {
+        start = std::clock();
+
+        // filter the sinogram
+        filterSinogram(sinogram, sinogramFiltered, sinogramHeight);
+
+        duration = std::clock() - start;
+        printf("sinogram filtering time = %.0f ms\n", duration);
+
+        cvimshow(sinogramFiltered, sinogramHeight, false, "sinogram");
+    }
 
     // number of parallel x-ray emitters/receivers
     const int totalProjections = sinogramHeight;
@@ -69,6 +121,9 @@ int main() {
 
     // find the middle index of the projections
     int midindex = int(bpWidth/2);
+
+
+    start = std::clock();
 
     // reconstruct pixel by pixel
     for (int r=0; r<bpWidth; r++) {
@@ -98,11 +153,18 @@ int main() {
                 // check if we are in boundaries
                 if ((projectionIdx > -1) && (projectionIdx < bpWidth)) {
                     // update the pixel's value
-                    backProjection[r][c] += sinogram[projectionIdx][sliceIdx]/totalSlices;
+                    if (filteredBP) {
+                        backProjection[r][c] += sinogramFiltered[projectionIdx][sliceIdx]/totalSlices;
+                    } else {
+                        backProjection[r][c] += sinogram[projectionIdx][sliceIdx]/totalSlices;
+                    }
                 }
             }
         }
     }
+
+    duration = std::clock() - start;
+    printf("reconstruction time = %.0f ms\n", duration);
 
     // save to file
     saveToFile(outputFile, backProjection, bpWidth);
@@ -113,6 +175,7 @@ int main() {
 
     return 0;
 }
+
 
 
 /**********************
@@ -174,4 +237,56 @@ void cvimshow (T mat[][cols], size_t rows, bool flip, std::string title, int wai
         cv::flip(im,im,0);
     }
     cv::imshow(title, im); cv::waitKey(waitKey);
+}
+
+template<typename T, size_t cols>
+void saveSinogramAsFile(std::string filename, T mat[][cols], size_t rows) {
+    std::ofstream fout;
+    fout.open(filename);
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
+            fout << mat[i][j] << " ";
+        }
+    }
+    fout.close();
+}
+
+void getFitler(float* a, int len) {
+    int n = int(-len/2);
+    for (int i=0; i<len; i++) {
+        a[i] = -2. / (pow(PI,2.) * (4 * pow(n+i,2.) - 1) );
+    }
+}
+
+void centralConvolution(float* a, float* b, float* c, int s) {
+    for (int i=0; i<s; i++) {
+          float val = 0;
+          for (int j=0; j<s; j++) {
+              val +=  a[(s-1)-j] * b[i+j+1];
+          }
+          c[i] = val;
+      }
+}
+
+template<typename T, size_t cols>
+void filterSinogram (T src[][cols], T dst[][cols], size_t rows) {
+    // get the filter
+    const int len = (sinogramHeight%2 == 0) ? (sinogramHeight+1)*2 + 1 : sinogramHeight*2 + 1; // lenght of filter
+    float filter[len];
+    getFitler(filter, len);
+    // convolve each column of the sinogram with the filter
+    for (int i=0; i<cols; i++) {
+        // get the ith column of the sinogram
+        T column[sinogramHeight];
+        for (int j=0; j<rows; j++) {
+            column[j] = src[j][i];
+        }
+        // convolve
+        T result[sinogramHeight];
+        centralConvolution(column, filter, result, rows);
+        // add to dst matrix
+        for (int j=0; j<rows; j++) {
+            dst[j][i] = result[j];
+        }
+    }
 }
